8cf5c3ee61777ffa15250d2e54a1618ba9c1ed5a
nehyshta <84226219+nehyshta@users.noreply.github.com>
2022-02-21 23:53:33 +0300

    Sixth cycle

---
+++ org/nehyshta/solutions/cycle/Sixth.java
@@ -0,0 +1,10 @@
+package org.nehyshta.solutions.cycle;
+
+public class Sixth {
+    public static void main(String[] args) {
+        for (int i = 32; i <= 126; i++) {
+            System.out.println(i + " " + (char) i);
+        }
+
+    }
+}
