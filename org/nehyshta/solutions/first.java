55886c4654958b81108eaabcd5e8ab2e713955c4
nehyshta <84226219+nehyshta@users.noreply.github.com>
2022-02-21 20:23:02 +0300

    completed (I hope right)

--- org/nehyshta/solutions/first.java
+++ org/nehyshta/solutions/first.java
@@ -1,5 +1,19 @@
 package org.nehyshta.solutions;
 
+import java.util.Scanner;
+
 public class first {
 
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        float a = s.nextInt();
+        float b = s.nextInt();
+        float c = s.nextInt();
+
+        float z = ((a-3)*b/2)+c;
+        System.out.printf("z is %.0f", z);
+    }
 }
+
