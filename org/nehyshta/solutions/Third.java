9685aad12bda160921526083cdc1afb604e07836
nehyshta <84226219+nehyshta@users.noreply.github.com>
2022-02-21 20:39:07 +0300

    third

---
+++ org/nehyshta/solutions/Third.java
@@ -0,0 +1,19 @@
+package org.nehyshta.solutions;
+
+import java.util.Scanner;
+
+public class Third {
+    public static void main(String[] args) {
+        float x, y;
+        Scanner scanner = new Scanner(System.in);
+        x= scanner.nextFloat();
+        y= scanner.nextFloat();
+
+        findSol(x,y);
+    }
+
+    public  static  void findSol(float x, float y){
+        float z = (float) (((Math.sin(x)+Math.cos(y))/Math.cos(x)-Math.sin(y))*Math.tan(x*y));
+        System.out.println(z);
+    }
+}
