d522de3650fff0e2cc5d94a6bc08e429b65a3037
nehyshta <84226219+nehyshta@users.noreply.github.com>
2022-02-21 20:33:25 +0300

    i did second and i hope i do everything right AAAAA

---
+++ org/nehyshta/solutions/Second.java
@@ -0,0 +1,20 @@
+package org.nehyshta.solutions;
+
+import java.util.Scanner;
+
+public class Second {
+    public static void main(String[] args) {
+        float a, b, c;
+        Scanner scanner = new Scanner(System.in);
+        a= scanner.nextFloat();
+        b= scanner.nextFloat();
+        c= scanner.nextFloat();
+        findSol(a,b,c);
+    }
+
+    public static void findSol(float a, float b, float c) {
+        float z = (float) (((b + Math.sqrt(Math.pow(b, 2) + 4 * a * c)) / 2 * a) - Math.pow(a, 3) * c + Math.pow(b, -2));
+
+        System.out.println(z);
+    }
+}
--- org/nehyshta/solutions/first.java
+++ org/nehyshta/solutions/first.java
@@ -8,9 +8,9 @@ public class first {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        float a = s.nextInt();
-        float b = s.nextInt();
-        float c = s.nextInt();
+        float a = s.nextFloat();
+        float b = s.nextFloat();
+        float c = s.nextFloat();
 
         float z = ((a-3)*b/2)+c;
         System.out.printf("z is %.0f", z);
